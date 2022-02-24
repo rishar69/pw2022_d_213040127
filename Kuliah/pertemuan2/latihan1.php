7ef16ed028124407f0d1cf6eaba1448daeecbfcd
rishar69 <harisharyarevano@gmail.com>
2022-02-24 10:52:10 +0700

    Belajar GIT

---
+++ Kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,34 @@
+<?php   
+// operator
+// aritmatika
+//  +, -, *, /, %
+
+echo 5 % 2 . "% = sisa bagi"; //  % = sisa bagi
+echo "<br>";
+
+// perbandingan 
+// <, >, <= , >=, ==, !=
+
+// Varibel 
+// Tidak boleh mengandung spasi
+// tidak boleh mengandung angka di awal
+// snake_case : $nama_depan_mahasiswa
+// camelcase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+$nama = "Harish";
+echo $nama ="Harish";
+echo "<br>";
+
+// assignment
+// Contoh : =, +=, -=, /=, %= 
+
+// increment & decrement
+// ++, -- 
+
+// operator identitas
+// ===, !==
+// untuk membandingkan nilai yang berbed jenis
+
+
+
+?>
