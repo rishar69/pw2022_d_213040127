96ebb038c0d4bd78269447ee496b3e1306c34de5
rishar69 <harisharyarevano@gmail.com>
2022-03-14 19:34:11 +0700

    Belum Mulai

---
+++ Kuliah/pertemuan5/latihan1.php
@@ -0,0 +1,51 @@
+<?Php 
+// // array adalah variabel yang dapat menyimpan banyak nilai sekaligus
+
+// // membuat array
+// $hari = "senin";
+// $hari1 = "selasa";
+// $hari2 = "rabu";
+
+// $hari = ["senin", "selasa", "rabu", "kamis", "jum'at", "sabtu", "minggu"];//versi baru
+
+// $bulan = array("januari", "februari", "maret");
+
+// // mencetak echo
+// // menggunakan index, dimulai dari 0
+// echo $hari[2];
+// echo "<br>";
+// echo $hari1[1];
+// echo "<br>";
+
+// // menggunakan var_dump atau print_r( ) untuk menectak semua value di array
+// // hanya untuk debugging
+
+// var_dump($hari);
+// echo "<br>";
+
+// print_r($hari);
+
+// echo "<br>";
+
+// // mencetak array untuk user
+// // menggunakan looping
+
+// for($i = 0; $i < 7; $i++){
+//     echo $hari[$i];
+//     echo "<br> ";
+// }
+// echo "<hr>";
+// // menggunakan foreach
+// // looping khusus array
+
+// foreach ($hari as $h){
+// echo $h . "<br>";
+// }
+// echo "<hr>";
+// // memanipulasi array
+// // menambah 1 element di akhir
+
+// $bulan[] = "april";
+// $bulan[] = "mei";
+// print_r($bulan)
+?>
---
+++ Kuliah/pertemuan5/latihan2.php
@@ -0,0 +1,20 @@
+<?php
+
+// // array multi dimensi
+// // array dalam array
+
+// $array = [10, "harish", true, [1,2,3,4]];
+// // mencetak array di dalam array
+// echo $array[3][1]."<hr>";
+
+// // matriks
+
+// $matriks = [[1,2,3],[4,5,6],[7,8,9]];
+
+// foreach ($matriks as $baris){
+//     foreach($baris as $kolom){
+//         echo $kolom;
+//     }
+//     echo"<br>";
+// }
+?>
